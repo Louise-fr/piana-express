d623b5a18ed9bc171427ee8c351debc690550892
louise <lou@git.fr>
2021-07-15 17:42:00 +0200

    server index.js file cleaned

--- server/index.js
+++ server/index.js
@@ -36,52 +36,7 @@ app.get("/send-email", cors(corsOptions),(req, res, next) => {
     res.json({"message":"use POST instead"})
 });
 
-// app.get("/api/users", (req, res, next) => {
-//     var sql = "select * from user"
-//     var params = []
-//     db.all(sql, params, (err, rows) => {
-//         if (err) {
-//           res.status(400).json({"error":err.message});
-//           return;
-//         }
-//         res.json({
-//             "message":"success",
-//             "data":rows
-//         })
-//       });
-// });
 
-// app.post("/api/user/", (req, res, next) => {
-//     var errors=[]
-//     if (!req.body.password){
-//         errors.push("No password specified");
-//     }
-//     if (!req.body.email){
-//         errors.push("No email specified");
-//     }
-//     if (errors.length){
-//         res.status(400).json({"error":errors.join(",")});
-//         return;
-//     }
-//     var data = {
-//         name: req.body.name,
-//         email: req.body.email,
-//         password : md5(req.body.password)
-//     }
-//     var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-//     var params =[data.name, data.email, data.password]
-//     db.run(sql, params, function (err, result) {
-//         if (err){
-//             res.status(400).json({"error": err.message})
-//             return;
-//         }
-//         res.json({
-//             "message": "success",
-//             "data": data,
-//             "id" : this.lastID
-//         })
-//     });
-// })
 
 // Insert here other API endpoints
 
